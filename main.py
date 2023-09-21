d567eda634747f6024e5977bbc2553238f7ca688
Yazdan Haghi <105206921+YazdanHaghi@users.noreply.github.com>
2023-09-21 12:11:37 +0330

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+# class person :
+#
+#     name = "yazdan"
+#     age = 23
+#
+#
+# person1 = person
+# person2 = person
+# person1.name = "yazdan"
+# print(person1.name)
+# person2.name= "mohammad"
+#
+#
+#
+# print(person2.name)
+# print(person2.age)
+# print(person1.name)
+
+
